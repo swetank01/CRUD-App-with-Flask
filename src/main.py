c6a016f789571845ed935676d4a98841d464a791
swetank01 <iamswetanksoni@gmail.com>
2021-04-23 10:04:00 +0530

    update docker-compose

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
 from flask import jsonify
+from api.utils.database import db
+from api.utils.responses import response_with
+import api.utils.responses as resp
 
 app = Flask(__name__)
 if os.environ.get('WORK_ENV') == 'PROD':
@@ -9,7 +12,34 @@ elif os.environ.get('WORK_ENV') == 'TEST':
     app_config = TestingConfig
 else:
     app_config = DevelopmentConfig
+
 app.config.from_object(app_config)
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# START GLOBAL HTTP CONFIGURATIONS
+@app.after_request
+def add_header(response):
+    return response
+
+@app.errorhandler(400)
+def bad_request(e):
+    logging.error(e)
+    return response_with(resp.BAD_REQUEST_400)
+@app.errorhandler(500)
+def server_error(e):
+    logging.error(e)
+    return response_with(resp.SERVER_ERROR_500)
+@app.errorhandler(404)
+def not_found(e):
+    logging.error(e)
+    return response_with(resp.SERVER_ERROR_404)
+    
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", use_reloader=False)
